fb2837e0f765f1f89fad85b9da0ce7ec7edf5313
Mahadi Ibne Bakar <105577484+Mahadi-Irl@users.noreply.github.com>
2024-12-06 16:04:21 +0600

    Advanced Setup
    Full Featured Web App Setup

--- flaskapp.py
+++ flaskapp.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+services = [
+    {
+        'title' : 'Cleaning House',
+        'service_providor' : 'Shakib',
+        'description' : 'Deep cleaning of the whole house',
+        'date_posted' : 'April 25, 2024'
+    },
+    {
+        'title' : 'Washing Toilet',
+        'service_providor' : 'Tamim',
+        'description' : 'Deep cleaning of the toilet',
+        'date_posted' : 'May 12, 2024'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', services=services)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
